2eb84f19bc999739131e55f2edf2ffc4da2c5a55
Allegra03 <allegraazharizain@gmail.com>
2021-10-11 08:19:52 +0700

    First Commit

---
+++ Dasar Pemrograman/Coding/Pertemuan 1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "Haloo Gaes" << endl;
+    cout << "KOK ERROR ?" << endl;
+    return;0
+}
---
+++ Dasar Pemrograman/Coding/Praktik 1/main.cpp
@@ -0,0 +1,19 @@
+//Judul: Praktik 1
+/*
+Ini komentar, tidak terbaca program
+Nama                    : Allegra Azhari Zain
+Tempat, Tanggal lahar   : Surakarta, 1 September 2003
+*/
+// \"untuk menambahkan petik \*
+// \\ untuk menulis \
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Apa itu \'Teknologi\'?" <<endl;
+    cout << "\"Ilmu yang berkaitan dengan pengetahuan sains \\ seni dengan mengaplikasikan pengetahuan saintifik ke praktis.\"" << endl;
+    return 0;
+}
